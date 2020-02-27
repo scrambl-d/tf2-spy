850f1189b59b6c5af44d6fab128a3f088233a566
unknown <calumjball@gmail.com>
2020-02-27 05:40:46 +0000

    Restructured to classes and added etf2l api filling

--- compinfo.user.js
+++ compinfo.user.js
@@ -1,22 +1,85 @@
 // ==UserScript==
 // @name		TF2 User Info
-// @match *://*.steamcommunity.com/id/*
-// @match *://*.steamcommunity.com/profiles/*
-// @match *://*.logs.tf/*
-// @match *://*.tf2center.com/*
+// @match		*://*.steamcommunity.com/id/*
+// @match		*://*.steamcommunity.com/profiles/*
+// @match		*://*.logs.tf/*
+// @match		*://*.tf2center.com/*
 // @namespace	https://github.com/scrambl-d/userinfo
-// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require		https://code.jquery.com/jquery-3.4.1.min.js
+// @require 	https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @grant		GM_getValue
+// @grant		GM.getValue
+// @grant		GM_setValue
+// @grant		GM.setValue
 // ==/UserScript==
 
 var domain = "";
-var htmlNameClass = "";
-var id64;
+
+class Players {
+	constructor() {
+		(async () => {
+			this.players = JSON.parse(await GM.getValue("cache", "{}")); 
+		})();
+		this.selectedPlayer = null;
+	}
+	
+	selectPlayer(id64) {
+		if (!this.players[id64]) {
+			this.players[id64] = new Player();
+		}
+		this.selectedPlayer = id64;
+	}
+	
+	populateETF2L() {
+		if (!this.players[this.selectedPlayer].etf2l.id) {
+			this.data = getETF2LData(this.selectedPlayer);
+			
+			this.players[this.selectedPlayer].etf2l.id = this.data.player.id;
+			this.players[this.selectedPlayer].etf2l.name = this.data.player.name;
+			this.players[this.selectedPlayer].etf2l.country = this.data.player.country;
+		}
+	}
+	
+	logData() {
+		console.log(this.players[this.selectedPlayer]);
+	}
+}
+
+class Player {
+	constructor() {
+		this.birth = Date.now();
+		
+		this.etf2l = {
+			id			: "",
+			name		: "",
+			country		: "",
+			
+			peak6v6 	: {},
+			current6v6	: {},
+			
+			peakHL		: {},
+			currentHL	: {}
+		}
+	}
+}
+
+class divInfo {
+	constructor(div, team, season) {
+		this.div = div;
+		this.team = team;
+		this.season = season;
+	}
+}
+
+players = new Players() 
 
 switch(document.location.hostname) {
 	case "steamcommunity.com":
 		domain = "steamcommunity";
-		id64 = getID64SteamCommunity();
-		console.log(id64);
+		var id64 = getSteamcommunityID64();
+		players.selectPlayer(id64);
+		players.populateETF2L();
+		players.logData();
 		break;
 	case "logs.tf":
 		domain = "logs.tf";
@@ -30,7 +93,7 @@ switch(document.location.hostname) {
 		console.log("domain not in switch");
 }
 
-function getID64SteamCommunity() {
+function getSteamcommunityID64() {
 	var id = 0;
 	// get steamcommunity.com/profiles/* or /id/* -- vanity uses id, non vanity uses profiles
 	var regex = /(?:^.{4,5}\:\/\/steamcommunity.com\/)([a-z]*)(?:.*)/i;
@@ -53,8 +116,12 @@ function getID64SteamCommunity() {
 	return id;
 }
 
-function httpGet(theUrl)
-{
+function getETF2LData(id64) {
+	var data = JSON.parse(httpGet("http://api.etf2l.org/player/" + id64 + ".json"));
+	return data;
+}
+
+function httpGet(theUrl) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
     xmlHttp.send( null );
